eb7953eb0a1a5463989b7aece62d325953afaff0
adricatena <adricatena@gmail.com>
2021-02-12 18:02:28 -0300

    importe y defini las vistas 'tortas' y 'tartas'

--- Proyecto1/Proyecto1/urls.py
+++ Proyecto1/Proyecto1/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Proyecto1.views import saludo, despedida, getFecha, calcularEdad
+from Proyecto1.views import saludo, despedida, getFecha, calcularEdad, tortas, tartas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +23,6 @@ urlpatterns = [
     path('nosvemos/', despedida),
     path('fecha/', getFecha),
     path('edad/<int:edad>/<int:anio>', calcularEdad),
+    path('tortas/', tortas),
+    path('tartas/', tartas),
 ]
