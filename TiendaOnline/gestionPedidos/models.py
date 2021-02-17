10b22ee3e85606e800b50629c6294e3252f07348
adricatena <adricatena@gmail.com>
2021-02-17 20:28:58 -0300

    add funcion para mostrar consulta como string

--- TiendaOnline/gestionPedidos/models.py
+++ TiendaOnline/gestionPedidos/models.py
@@ -15,6 +15,9 @@ class Articulos(models.Model):
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField()
 
+    def __str__(self):
+        return 'El nombre es: %s, la seccion es %s y el precio es %d' % (self.nombre, self.seccion, self.precio)
+
 
 class Pedidos(models.Model):
     numero = models.IntegerField()
