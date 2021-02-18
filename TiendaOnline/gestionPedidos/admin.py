38ac5519ef27c57de60c9d4120eb5b0b1d7c2736
adricatena <adricatena@gmail.com>
2021-02-18 11:21:06 -0300

    creacion de clase para listar campos a mostrar

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -3,6 +3,11 @@ from gestionPedidos.models import Clientes, Articulos, Pedidos
 
 # Register your models here.
 
-admin.site.register(Clientes)
+
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "direccion", "telefono")
+
+
+admin.site.register(Clientes, ClientesAdmin)
 admin.site.register(Articulos)
 admin.site.register(Pedidos)
