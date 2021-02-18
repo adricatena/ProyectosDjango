3b98cd83f48079b02c5827926117bfd7748c5bcd
adricatena <adricatena@gmail.com>
2021-02-18 19:03:32 -0300

    defini views de busquedaProductos y buscar

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,3 +1,14 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def busquedaProductos(request):
+    return render(request, "busqueda_productos.html")
+
+
+def buscar(request):
+    mensaje = "Articulo buscado: %r" % request.GET["producto"]
+    return HttpResponse(mensaje)
