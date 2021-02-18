a0da7859f47b31f1e6610d31a149d1e28da3fe3b
adricatena <adricatena@gmail.com>
2021-02-18 19:56:31 -0300

    add metodos para renderizar la info de la consulta

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestionPedidos.models import Articulos
 
 # Create your views here.
 
@@ -10,5 +11,11 @@ def busquedaProductos(request):
 
 
 def buscar(request):
-    mensaje = "Articulo buscado: %r" % request.GET["producto"]
+    if request.GET["producto"]:
+        #mensaje = "Articulo buscado: %r" % request.GET["producto"]
+        producto = request.GET['producto']
+        articulos = Articulos.objects.filter(nombre__icontains=producto)
+        return render(request, 'resultados_busqueda.html', {"articulos": articulos, 'query': producto})
+    else:
+        mensaje = "No has introducido nada a buscar"
     return HttpResponse(mensaje)
