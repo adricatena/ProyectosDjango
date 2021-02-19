7acb000c380e290dee6267dddb45c83b69f3b48f
adricatena <adricatena@gmail.com>
2021-02-19 18:55:20 -0300

    modificacion view contacto para API form

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from gestionPedidos.models import Articulos
 from django.core.mail import send_mail
 from django.conf import settings
+from gestionPedidos.forms import formularioContacto
 
 # Create your views here.
 
@@ -28,11 +29,22 @@ def buscar(request):
 
 def contacto(request):
     if request.method == 'POST':
-        subject = request.POST['asunto']
-        message = request.POST['mensaje'] + ' ' + request.POST['email']
-        email_from = settings.EMAIL_HOST_USER
-        recipient_list = ['adricatena@gmail.com']
-        send_mail(subject, message, email_from, recipient_list)
-        return render(request, 'gracias.html')
+        miFormulario = formularioContacto(request.POST)
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
+            send_mail(informacion['asunto'], informacion['mensaje'], informacion.get(
+                'email', ''), ['adricatena@gmail.com'])
+            return render(request, 'gracias.html')
     else:
-        return render(request, 'contacto.html')
+        miFormulario = formularioContacto()
+    return render(request, 'formulario_contacto.html', {'form': miFormulario})
+
+    # if request.method == 'POST':
+    #     subject = request.POST['asunto']
+    #     message = request.POST['mensaje'] + ' ' + request.POST['email']
+    #     email_from = settings.EMAIL_HOST_USER
+    #     recipient_list = ['adricatena@gmail.com']
+    #     send_mail(subject, message, email_from, recipient_list)
+    #     return render(request, 'gracias.html')
+    # else:
+    #     return render(request, 'contacto.html')
