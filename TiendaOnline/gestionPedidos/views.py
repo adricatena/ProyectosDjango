1d75c32425304eabcdd6ba78a2b3f0394dc0350f
adricatena <adricatena@gmail.com>
2021-02-19 17:30:13 -0300

    new view para form de contacto y envio de mail

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -2,6 +2,8 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from gestionPedidos.models import Articulos
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
@@ -14,8 +16,23 @@ def buscar(request):
     if request.GET["producto"]:
         #mensaje = "Articulo buscado: %r" % request.GET["producto"]
         producto = request.GET['producto']
-        articulos = Articulos.objects.filter(nombre__icontains=producto)
-        return render(request, 'resultados_busqueda.html', {"articulos": articulos, 'query': producto})
+        if len(producto) > 20:
+            mensaje = 'Texto de busqueda demasiado largo'
+        else:
+            articulos = Articulos.objects.filter(nombre__icontains=producto)
+            return render(request, 'resultados_busqueda.html', {"articulos": articulos, 'query': producto})
     else:
         mensaje = "No has introducido nada a buscar"
     return HttpResponse(mensaje)
+
+
+def contacto(request):
+    if request.method == 'POST':
+        subject = request.POST['asunto']
+        message = request.POST['mensaje'] + ' ' + request.POST['email']
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ['adricatena@gmail.com']
+        send_mail(subject, message, email_from, recipient_list)
+        return render(request, 'gracias.html')
+    else:
+        return render(request, 'contacto.html')
