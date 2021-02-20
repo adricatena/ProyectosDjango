9e3cd5d41bf7ed5848780b6ceb9dbf0963e3490b
adricatena <adricatena@gmail.com>
2021-02-20 18:21:03 -0300

    new app y templates del ejemplo

--- ProyectoWeb/ProyectoWebApp/views.py
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -4,20 +4,20 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return render(request, 'ProyectoWebApp/home.html')
+    return render(request, 'ProyectoWebApp/home', {'nombre': 'home'})
 
 
 def servicios(request):
-    return render(request, 'ProyectoWebApp/servicios.html')
+    return render(request, 'ProyectoWebApp/servicios', {'nombre': 'servicios'})
 
 
 def tienda(request):
-    return render(request, 'ProyectoWebApp/tienda.html')
+    return render(request, 'ProyectoWebApp/tienda')
 
 
 def blog(request):
-    return render(request, 'ProyectoWebApp/blog.html')
+    return render(request, 'ProyectoWebApp/blog')
 
 
 def contacto(request):
-    return render(request, 'ProyectoWebApp/contacto.html')
+    return render(request, 'ProyectoWebApp/contacto')
