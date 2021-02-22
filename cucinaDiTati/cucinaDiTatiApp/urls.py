de90539092c99951e93448c67a85587da376a8d2
adricatena <adricatena@gmail.com>
2021-02-22 13:50:25 -0300

    add jquery for bootstrap and  active nav

--- cucinaDiTati/cucinaDiTatiApp/urls.py
+++ cucinaDiTati/cucinaDiTatiApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from cucinaDiTatiApp import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name=''),
     path('home/', views.home, name='home'),
     path('servicios/', views.servicios, name='servicios'),
     path('tienda/', views.tienda, name='tienda'),
