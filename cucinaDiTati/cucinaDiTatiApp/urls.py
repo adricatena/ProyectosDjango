989b9275b3980234d69b518bed01803be9d0a622
adricatena <adricatena@gmail.com>
2021-02-22 19:19:40 -0300

    delete url de app 'servicios'

--- cucinaDiTati/cucinaDiTatiApp/urls.py
+++ cucinaDiTati/cucinaDiTatiApp/urls.py
@@ -4,7 +4,6 @@ from cucinaDiTatiApp import views
 urlpatterns = [
     path('', views.home, name=''),
     path('home/', views.home, name='home'),
-    path('servicios/', views.servicios, name='servicios'),
     path('tienda/', views.tienda, name='tienda'),
     path('contacto/', views.contacto, name='contacto'),
     path('blog/', views.blog, name='blog'),
