253f71f58e3979b752e672cf9e91fdf66cd0c14a
adricatena <adricatena@gmail.com>
2021-02-22 19:19:58 -0300

    delete view de app 'servicios'

--- cucinaDiTati/cucinaDiTatiApp/views.py
+++ cucinaDiTati/cucinaDiTatiApp/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
-from servicios.models import Servicio
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'cucinaDiTatiApp/home.html')
-
-
-def servicios(request):
-    servicios = Servicio.objects.all()
-    return render(request, 'cucinaDiTatiApp/servicios.html', {'servicios': servicios})
 
 
 def tienda(request):
