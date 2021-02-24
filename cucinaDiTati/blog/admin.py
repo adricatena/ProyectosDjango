81b30aa8c2addf64eea6df0611193efcb39376d5
adricatena <adricatena@gmail.com>
2021-02-24 17:48:34 -0300

    register models 'categoria' and 'post'

--- cucinaDiTati/blog/admin.py
+++ cucinaDiTati/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from blog import models
 
 # Register your models here.
+
+
+class CategoriaAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+admin.site.register(models.Categoria, CategoriaAdmin)
+admin.site.register(models.Post, PostAdmin)
