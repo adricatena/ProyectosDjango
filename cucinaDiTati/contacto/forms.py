e19f3bc9213ae4fd369684a5907801599e674fab
adricatena <adricatena@gmail.com>
2021-02-26 16:02:48 -0300

    edit form adding 'textarea;

--- cucinaDiTati/contacto/forms.py
+++ cucinaDiTati/contacto/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class FormularioContacto(forms.Form):
     nombre = forms.CharField(label='Nombre', required=True)
     email = forms.EmailField(label='Email', required=True)
-    contenido = forms.CharField(label='Contenido')
+    mensaje = forms.CharField(widget=forms.Textarea)
