533bb5e6a4ca328e56c649ae04fec30030f795f9
Adriano <57508409+adricatena@users.noreply.github.com>
2021-04-14 09:21:58 -0300

    add functionality to carro class

--- cucinaDiTati/carro/carro.py
+++ cucinaDiTati/carro/carro.py
@@ -3,3 +3,46 @@ class Carro:
         self.request = request
         self.session = request.session
         carro = self.session.get('carro')
+        if not carro:
+            carro = self.session = {}
+        else:
+            self.carro = carro
+
+    def agregar(self, producto):
+        if(str(producto.id) not in self.carro.keys()):
+            self.carro[producto.id] = {
+                "producto.id": producto.id,
+                "nombre": producto.nombre,
+                "precio": str(producto.precio),
+                "cantidad": 1,
+                "imagen": producto.imagen.url
+            }
+        else:
+            for key, value in self.carro.items():
+                if key == str(producto.id):
+                    value["cantidad"] = value["cantidad"]+1
+                    break
+        self.guardarCarro()
+
+    def guardarCarro(self):
+        self.session["carro"] = self.carro
+        self.session.modified = True
+
+    def eliminar(self, producto):
+        producto.id = str(producto.id)
+        if producto.id in self.carro:
+            del self.carro[producto.id]
+            self.guardarCarro()
+
+    def restar(self, producto):
+        for key, value in self.carro.items():
+            if key == str(producto.id):
+                value["cantidad"] = value["cantidad"]-1
+                if value["cantidad"] < 1:
+                    self.eliminar(producto)
+                break
+        self.guardarCarro()
+
+    def limpiarCarro(self):
+        self.session["carro"] = {}
+        self.session.modified = True
