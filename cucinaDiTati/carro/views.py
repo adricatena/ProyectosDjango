8443ffee89a8df7ffa34d953ce1144affdd2027c
Adriano <57508409+adricatena@users.noreply.github.com>
2021-04-14 20:01:46 -0300

    add views (controller) for carro

--- cucinaDiTati/carro/views.py
+++ cucinaDiTati/carro/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .carro import Carro
+from tienda.models import Producto
+from django.shortcuts import redirect
 
 # Create your views here.
+
+
+def agregarProducto(request, productoId):
+    carro = Carro(request)
+    producto = Producto.objects.get(id=productoId)
+    carro.agregar(producto=producto)
+    return redirect("tienda")
+
+
+def eliminarProducto(request, productoId):
+    carro = Carro(request)
+    producto = Producto.objects.get(id=productoId)
+    carro.eliminar(producto=producto)
+    return redirect("tienda")
+
+
+def restarProducto(request, productoId):
+    carro = Carro(request)
+    producto = Producto.objects.get(id=productoId)
+    carro.restar(producto=producto)
+    return redirect("tienda")
+
+
+def limpiarCarro(request):
+    carro = Carro(request)
+    carro.limpiarCarro()
+    return redirect("tienda")
